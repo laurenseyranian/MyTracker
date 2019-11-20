9d0ebd660dc0b99aaf07b59bd632d094b00b7882
laurenseyranian <lauren.seyranian@gmail.com>
2019-11-20 13:43:41 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const flash = require('express-flash');
+const bodyParser = require('body-parser');
+const path = require('path')
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static( __dirname + '/public/dist/public' ));
+
+
+mongoose.connect('mongodb://localhost/tracker_db', { useNewUrlParser: true });
+
+require('./server/models/models.js')
+require('./server/config/mongoose.js')
+require('./server/config/routes.js')(app)
+
+
+app.all("*", (req,res,next) => {
+    res.sendFile(path.resolve("./public/dist/public/index.html"))});
+app.listen(8000, () => console.log("listening on port 8000"));
---
+++ server/config/routes.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+Tracker = mongoose.model('Tracker');
+const controller = require('../controller/controller.js')
+
+module.exports = function(app) {
+
+}
