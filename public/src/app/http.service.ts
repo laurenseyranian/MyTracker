729acb55fe7673e952ace9dd16561eb047ce03b3
laurenseyranian <lauren.seyranian@gmail.com>
2019-11-20 17:08:09 -0800

    added home component

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+    constructor(private _http: HttpClient) { }
+
+    homePage() {
+        return this._http.get('/home_json');
+    }
 }
