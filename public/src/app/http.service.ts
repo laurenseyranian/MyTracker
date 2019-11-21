ce428325314f34e4e23004e5759a3ad100814eb9
Lindsey Vaughn <lindsey.l.vaughn@gmail.com>
2019-11-20 17:31:14 -0800

    new changes

--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class HttpService {
 
+<<<<<<< HEAD
   constructor() {}
 
 calendarPage(){
   return this._http.get('/calendar_json');
 }
 
+=======
+    constructor(private _http: HttpClient) { }
+
+    homePage() {
+        return this._http.get('/home_json');
+    }
+>>>>>>> 729acb55fe7673e952ace9dd16561eb047ce03b3
 }
