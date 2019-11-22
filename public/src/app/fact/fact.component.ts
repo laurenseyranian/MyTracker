120682b7054ffd3e1c6f1a8a6e3e9ccd177f9522
laurenseyranian <lauren.seyranian@gmail.com>
2019-11-21 20:57:17 -0800

    worked on fact component

--- public/src/app/fact/fact.component.ts
+++ public/src/app/fact/fact.component.ts
@@ -3,18 +3,21 @@ import { HttpClient } from '@angular/common/http';
 
 
 @Component({
-  selector: 'app-fact',
-  templateUrl: './fact.component.html',
-  styleUrls: ['./fact.component.css']
+    selector: 'app-fact',
+    templateUrl: './fact.component.html',
+    styleUrls: ['./fact.component.css']
 })
 export class FactComponent implements OnInit {
 
-constructor(private _http: HttpClient) { }
+    constructor(private _http: HttpClient) { }
 
+    articles: any;
 
-  ngOnInit() {
-    // var data = this._http.get('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=https://api.nytimes.com/svc/search/v2/articlesearch.json?q=beauty&api-key=3vV007lbGYtAKmPPk8lHB7wvZd2ddeSD')
-    //   console.log(data)
-  }
+    ngOnInit() {
+        this._http.get('https://api.nytimes.com/svc/search/v2/articlesearch.json?q=food&api-key=I6fIUq30TyULsrLIjHVC0z0tmIl79dob').subscribe(data => { 
+        console.log(data);
+        this.articles = data
+        })
+    }
 
 }
